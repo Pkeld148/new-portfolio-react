d5ee756a13944aafaba0c83e8c3f1e8a9c38790e
Paul Keldsen <Pkeld148@gmail.com>
2021-09-16 11:53:33 -0400

    created home and navbar components.  added materialize

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SUP YALL ITS ME ITS YA BOI</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
